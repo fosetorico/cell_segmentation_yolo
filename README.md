e8a747c18e0536a120e5a61e8f2ff6b5eb01be13
unknown <vallee.neutral@gmail.com>
2024-12-11 13:27:34 +0000

    data ingestion added

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # End-to-End Cell Segmentation Using Yolo 
+
+## Workflows
+
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from cellSegmentation.logger import logging
-from cellSegmentation.exception import AppException
-import sys
+import sys,os
+from cellSegmentation.pipeline.training_pipeline import TrainPipeline
 
-logging.info("welcome!!!")
 
-try:
-    a = 4/0
-except Exception as e:
-    raise AppException(e, sys)
+obj = TrainPipeline()
+obj.run_pipeline()
+print("Training Successfull!!") 
